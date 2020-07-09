9a0df0f0141c71dd7dc16e8e33037e014797ddb4
Dartware <dartware@mail.ru>
2020-07-09 19:33:48 +0300

    Create radio engine factory.

--- NRadio/RadioEngine.cs
+++ NRadio/RadioEngine.cs
@@ -3,7 +3,13 @@
 	/// <summary>
 	/// Radio-engine implementation based on the cross-platform library bass.dll. Requires: bass.dll - Bass Audio Library - available <see href="http://www.un4seen.com">here</see>.
 	/// </summary>
-	public sealed partial class RadioEngine : IRadioEngine
+	internal sealed partial class RadioEngine : IRadioEngine
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RadioEngine"/> class.
+		/// </summary>
+		internal RadioEngine()
+		{
+		}
 	}
 }
---
+++ NRadio/RadioEngineFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Dartware.NRadio
+{
+	/// <summary>
+	/// Provides access to factory properties and methods for creating and configuring <see cref="IRadioEngine"/> implementation instances.
+	/// </summary>
+	public sealed class RadioEngineFactory
+	{
+		/// <summary>
+		/// Gets the default <see cref="IRadioEngine"/> implementation instance for this factory.
+		/// </summary>
+		public static IRadioEngine Engine => new RadioEngine();
+	}
+}
